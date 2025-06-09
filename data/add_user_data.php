9cd5ffd02e507628efe7c03ea4e272f186f1ac52
DineshraTR <rajap.dinukad@gmail.com>
2025-06-09 10:56:38 +0530

    update

--- data/add_user_data.php
+++ data/add_user_data.php
@@ -6,16 +6,17 @@ $date = date("Y-m-d");
 $ful_name = $_POST["full_name"];
 $mobile = $_POST["mobile"];
 $email = $_POST["email"];
+$tin_number = $_POST["tin_number"];
 $user_type = $_POST["user_type"]; 
 
-$sql = "INSERT INTO user(f_name,mobile,email,reg_date,user_type) "
-    . "VALUES(\"$ful_name\",\"$mobile\",\"$email\",\"$date\",\"$user_type\")";
+$sql = "INSERT INTO user(f_name,mobile,email,reg_date,tin_number,user_type) "
+    . "VALUES(\"$ful_name\",\"$mobile\",\"$email\",\"$date\",\"$tin_number\",\"$user_type\")";
 
 if (mysqli_query($con, $sql)) {
     $last_id = mysqli_insert_id($con);
 
-    if($user_type == "person"){
-        header('Location: ../form.php?cu_id=' . $last_id);
+    if($user_type == 1){
+        header('Location: ../add_income_sources.php?cu_id=' . base64_encode($last_id));
     }
     
 
