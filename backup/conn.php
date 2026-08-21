b20a468c93373d0f78805e212fa891a6208237e5
KaviduDilshan <dilshank404@gmail.com>
2026-08-21 08:05:39 +0530

    bug fix

--- backup/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-    date_default_timezone_set("Asia/Colombo");
-
-    $con = mysqli_connect('localhost', 'u914971268_tax_calu', 'Tax1@tritcal.com', 'u914971268_tax_cal') or die('Unable To connect');
-?>
--- conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-    date_default_timezone_set("Asia/Colombo");
-
-    $con = mysqli_connect('localhost', 'root', '', 'hat') or die('Unable To connect');
-?>
